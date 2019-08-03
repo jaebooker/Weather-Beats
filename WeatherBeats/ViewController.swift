3abb8722058156d7267c727a321cb3a217f3f715
jaebooker <jaesonbooker@gmail.com>
2019-08-02 18:08:38 -0700

    Initial Commit

---
+++ WeatherBeats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherBeats
+//
+//  Created by Jaeson Booker on 8/2/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
