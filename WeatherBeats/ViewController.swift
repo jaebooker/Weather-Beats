fc826ca583b8e7b96400c743594c780bfc4d7467
jaebooker <jaesonbooker@gmail.com>
2019-08-04 12:37:36 -0700

    more blood

--- WeatherBeats/ViewController.swift
+++ WeatherBeats/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    var apiKey: Int = 0
+    var lat: Double = 0
+    var longi: Double = 0
+    var locationManager = CLLocationManager()
     @IBAction func happyButtonPressed(_ sender: Any) {
     }
     @IBAction func sadButtonPressed(_ sender: Any) {
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        if locationManager.location?.coordinate != nil {
+            lat = (locationManager.location?.coordinate.latitude)!
+            longi = (locationManager.location?.coordinate.longitude)!
+            print(lat)
+            print(longi)
+        }
+        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=" + String(lat) + "&lon=" + String(longi) + "&APPID=" + String(apiKey)) else { return }
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            do {
+                let apiEvents = try JSONDecoder().decode(Weather.self, from: data)
+                print(apiEvents)
+            } catch { }
+        }
+        task.resume()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
--- WeatherBeats/Weather.swift
+++ WeatherBeats/Weather.swift
@@ -9,5 +9,55 @@
 import Foundation
 
 struct Weather: Decodable {
+    var coord: Coord
+    var weather: [WeatherDescription]
+    var base: String
+    var main: Main
+    var visibility: Int
+    var wind: Wind
+    var clouds: Clouds
+    var dt: Int
+    var sys: Sys
+    var timezone: Int
+    var id: Int
+    var name: String
+    var cod: Int
+}
+
+struct Coord: Decodable {
+    var lon: Double
+    var lat: Double
+}
+
+struct WeatherDescription: Decodable {
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+}
+
+struct Main: Decodable {
+    var temp: Double
+    var pressure: Int
+    var humidity: Int
+    var temp_min: Double
+    var temp_max: Double
+}
+
+struct Wind: Decodable {
+    var speed: Float
+    var deg: Float
+}
+
+struct Clouds: Decodable {
     
+}
+
+struct Sys: Decodable {
+    var type: Int
+    var id: Int
+    var message: Float
+    var country: String
+    var sunrise: Int
+    var sunset: Int
 }
